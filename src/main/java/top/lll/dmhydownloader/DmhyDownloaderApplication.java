a7926841bab0d77b8a2e2b1397b4785fef3ecad2
liululu <liululu44556@163.com>
2024-12-02 16:35:46 +0800

    init<*> init spring boot project

---
+++ src/main/java/top/lll/dmhydownloader/DmhyDownloaderApplication.java
@@ -0,0 +1,13 @@
+package top.lll.dmhydownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmhyDownloaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmhyDownloaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/lll/dmhydownloader/DmhyDownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package top.lll.dmhydownloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmhyDownloaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
